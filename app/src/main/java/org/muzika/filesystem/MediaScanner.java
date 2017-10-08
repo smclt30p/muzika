60d89a8af02a31cee3dfe1d569d12e1a1bf449c4
Ognjen Galić <smclt30p@gmail.com>
2017-10-08 10:40:42 +0200

    Implement track classification

--- app/src/main/java/org/muzika/filesystem/MediaScanner.java
+++ app/src/main/java/org/muzika/filesystem/MediaScanner.java
@@ -26,15 +26,21 @@ package org.muzika.filesystem;
 */
 
 import android.os.AsyncTask;
+
+import org.muzika.model.Album;
+import org.muzika.model.Artist;
+import org.muzika.model.Library;
 import org.muzika.model.Track;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * The MediaScanner scans the storage media and returns supported files
  * as Tracks.
  */
-public class MediaScanner extends AsyncTask<Void, Void, ArrayList<Track>> {
+public class MediaScanner extends AsyncTask<Void, Void, Library> {
 
     /**
      * According to WikiPedia, FMOD supports  AIFF, ASF, ASX, DLS, FLAC,
@@ -53,10 +59,16 @@ public class MediaScanner extends AsyncTask<Void, Void, ArrayList<Track>> {
             "xm"
     };
 
+    enum SearchType {
+        ALBUM, ARTIST
+    }
+
+    private static final int FIND_NOT_FOUND = -2;
+
     private ArrayList<Track> walkedFiles = new ArrayList<>();
     private MediaScannerFinishedListener listener;
     private TrackMetadataReader metadataReader = new TrackMetadataReader();
-
+    private volatile Library library = new Library();
     private final String root;
 
     /**
@@ -68,22 +80,85 @@ public class MediaScanner extends AsyncTask<Void, Void, ArrayList<Track>> {
     }
 
     /**
+     * Create a new album from a track
+     * @param track the track to create the album for
+     * @return the new album
+     */
+    private Album createAlbum(Track track) {
+        Album album = new Album();
+        album.setName(track.getAlbum());
+        album.setYear(track.getYear());
+        album.setDescription(track.getAlbum());
+        return album;
+    }
+
+    /**
      * This is the worker thread entry point.
      */
     @Override
-    protected ArrayList<Track> doInBackground(Void... voids) {
-        walk(new File(root));
-        return walkedFiles;
+    protected Library doInBackground(Void... voids) {
+
+        File root = new File(this.root);
+
+        if (!root.exists() || !root.canRead()) return null;
+
+        walk(root);
+
+        for (Track track : walkedFiles) {
+
+            library.getTracks().add(track);
+
+            if (track.getArtist().equalsIgnoreCase("") ||
+                track.getTitle().equalsIgnoreCase("") ||
+                track.getAlbum().equalsIgnoreCase("")) {
+                library.getInvalidTags().add(track);
+                continue;
+            }
+
+            int artistIndex = find(library.getArtists(), track.getArtist(), SearchType.ARTIST);
+            if (artistIndex == FIND_NOT_FOUND) {
+                /* artist was not found */
+                Artist artist = new Artist();
+                artist.setName(track.getArtist());
+
+                Album album = createAlbum(track);
+
+                album.getTracks().add(track);
+                artist.getAlbums().add(album);
+
+                library.getArtists().add(artist);
+                library.getAlbums().add(album);
+
+            } else {
+                /* artist was found */
+                Artist artist = library.getArtists().get(artistIndex);
+                int albumIndex = find(artist.getAlbums(), track.getAlbum(), SearchType.ALBUM);
+
+                if (albumIndex == FIND_NOT_FOUND) {
+                    /* album not found */
+                    Album album = createAlbum(track);
+                    album.getTracks().add(track);
+                    artist.getAlbums().add(album);
+                    library.getAlbums().add(album);
+                } else {
+                    /* album found */
+                    Album album = artist.getAlbums().get(albumIndex);
+                    album.getTracks().add(track);
+                }
+            }
+        }
+
+        return library;
     }
 
     /**
      * This executes after the worker thread has finished
      * walking the directory.
-     * @param tracks the supported tracks found
+     * @param library the whole library that was scanned
      */
     @Override
-    protected void onPostExecute(ArrayList<Track> tracks) {
-        listener.publishMedia(tracks);
+    protected void onPostExecute(Library library) {
+        listener.publishMedia(library);
     }
 
     /**
@@ -98,26 +173,27 @@ public class MediaScanner extends AsyncTask<Void, Void, ArrayList<Track>> {
             if (file.isDirectory()) {
                 walk(file);
             }
-            System.out.println(file.getAbsolutePath());
+
             if (!isFileSupported(file)) continue;
             Track track = new Track();
             track.setFile(file);
 
             if (metadataReader.openFile(file.getAbsolutePath()) != 0) {
                 System.err.println("Unsupported file by TagLib: " + file.getAbsolutePath());
-                walkedFiles.add(track);
+                library.getInvalidTags().add(track);
+                metadataReader.close();
                 continue;
             }
 
-            System.out.println(String.format(
-                    "Song: %s\n" +
-                    "Artist: %s\n" +
-                    "Album: %s\n",
-                    metadataReader.getTitle(),
-                    metadataReader.getArtist(),
-                    metadataReader.getAlbum()
-            ));
-
+            track.setTitle(metadataReader.getTitle());
+            track.setArtist(metadataReader.getArtist());
+            track.setAlbum(metadataReader.getAlbum());
+            track.setComment(metadataReader.getComment());
+            track.setBitrate(metadataReader.getBitRate());
+            track.setTrackNumber(metadataReader.getTrackNumber());
+            track.setSamplerate(metadataReader.getSampleRate());
+            track.setYear(metadataReader.getYear());
+            track.setLength(metadataReader.getLength());
 
             metadataReader.close();
             walkedFiles.add(track);
@@ -149,6 +225,40 @@ public class MediaScanner extends AsyncTask<Void, Void, ArrayList<Track>> {
      */
     public void addFinishListener(MediaScannerFinishedListener finishedListener) {
         this.listener = finishedListener;
+    }
+
+    /**
+     * Search for an item in a list based on name
+     * @param haystack the haystack of items
+     * @param needle the needle to find
+     * @param type the type of search
+     * @return the index of the item in the list
+     */
+    private int find(List haystack, String needle, SearchType type) {
+
+        for (int i = 0; i < haystack.size(); i++) {
+
+            switch (type) {
+                case ALBUM:
+                    Album album = (Album) haystack.get(i);
+                    if (album.getName().equalsIgnoreCase(needle)) {
+                        return i;
+                    }
+                    break;
+                case ARTIST:
+                    Artist artist = (Artist) haystack.get(i);
+                    if (artist.getName().equalsIgnoreCase(needle)) {
+                        return i;
+                    }
+                    break;
+                default:
+                    throw new RuntimeException("Invalid search flag!" + type);
+            }
+
+        }
+
+        return FIND_NOT_FOUND;
+
     }
 
 }
