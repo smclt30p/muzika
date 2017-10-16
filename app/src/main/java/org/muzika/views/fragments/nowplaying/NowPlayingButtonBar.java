519fcf675123c2dcf98cacdd5ca053d2c410d726
Ognjen Galić <smclt30p@gmail.com>
2017-10-16 20:54:11 +0200

    ui: implement state management to button bar

--- app/src/main/java/org/muzika/views/fragments/nowplaying/NowPlayingButtonBar.java
+++ app/src/main/java/org/muzika/views/fragments/nowplaying/NowPlayingButtonBar.java
@@ -32,11 +32,158 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import org.muzika.R;
+import org.muzika.views.basic.SquareBitmapButton;
 
-public class NowPlayingButtonBar extends RelativeLayout {
+import java.util.ArrayList;
+
+public class NowPlayingButtonBar extends RelativeLayout implements Button.OnClickListener {
+
+    private SquareBitmapButton playButton;
+    private SquareBitmapButton repeatButton;
+    private SquareBitmapButton randomButton;
+    private SquareBitmapButton nextButton;
+    private SquareBitmapButton previousButton;
+
+    /**
+     * Button click inside navigation bar
+     */
+    @Override
+    public void onClick(View view) {
+
+        if (view == playButton) {
+            playPressed();
+        } else if (view == repeatButton) {
+            repeatPressed();
+        } else if (view == randomButton) {
+            randomPressed();
+        } else if (view == nextButton) {
+            nextPressed();
+        } else if (view == previousButton) {
+            previousPressed();
+        }
+
+    }
+
+    private void playPressed() {
+
+        switch (playStatus) {
+            case PAUSED:
+                playStatus = PlayStatus.PLAYING;
+                playButton.setBackgroundResource(R.drawable.icons8pause96);
+                break;
+            case PLAYING:
+                playButton.setBackgroundResource(R.drawable.icons8play96);
+                playStatus = PlayStatus.PAUSED;
+                break;
+        }
+
+        for (OnClickListener listener : listeners) {
+            listener.playPressed(playStatus);
+        }
+
+    }
+
+    private void repeatPressed() {
+
+        switch (repeatStatus) {
+            case NO_REPEAT:
+                repeatStatus = RepeatStatus.REPEAT_PLAYLIST;
+                repeatButton.setBackgroundResource(R.drawable.icons8repeat96);
+                repeatButton.setAlpha(1);
+                break;
+            case REPEAT_PLAYLIST:
+                repeatStatus = RepeatStatus.REPEAT_ONE;
+                repeatButton.setBackgroundResource(R.drawable.icons8repeatone96);
+                break;
+            case REPEAT_ONE:
+                repeatStatus = RepeatStatus.NO_REPEAT;
+                repeatButton.setBackgroundResource(R.drawable.icons8repeat96);
+                repeatButton.setAlpha(0.5f);
+                break;
+        }
+
+        for (OnClickListener listener : listeners) {
+            listener.repeatPressed(repeatStatus);
+        }
+        
+    }
+
+    private void randomPressed() {
+
+        switch (randomStatus) {
+            case SEQUENTIAL:
+                randomStatus = RandomStatus.RANDOM;
+                randomButton.setAlpha(1);
+                break;
+            case RANDOM:
+                randomStatus = RandomStatus.SEQUENTIAL;
+                randomButton.setAlpha(0.5f);
+                break;
+        }
+
+        for (OnClickListener listener : listeners) {
+            listener.randomPressed(randomStatus);
+        }
+
+    }
+
+    private void nextPressed() {
+        for (OnClickListener listener : listeners) {
+            listener.nextPressed();
+        }
+    }
+
+    private void previousPressed() {
+        for (OnClickListener listener : listeners) {
+            listener.previousPressed();
+        }
+    }
+
+    public enum PlayStatus {
+        PLAYING, PAUSED
+    }
+
+    public enum RepeatStatus {
+        NO_REPEAT, REPEAT_PLAYLIST, REPEAT_ONE
+    }
+
+    public enum RandomStatus {
+        SEQUENTIAL, RANDOM
+    }
+
+    private PlayStatus playStatus = PlayStatus.PAUSED;
+    private RepeatStatus repeatStatus = RepeatStatus.NO_REPEAT;
+    private RandomStatus randomStatus = RandomStatus.SEQUENTIAL;
+    private final ArrayList<OnClickListener> listeners = new ArrayList<>();
+
+    public void addOnClickListener(OnClickListener listener) {
+        listeners.add(listener);
+    }
+
+    public void setPlayingStatus(PlayStatus status) {
+        this.playStatus = status;
+    }
+
+    public void setRepeatStatus(RepeatStatus status) {
+        this.repeatStatus = status;
+
+        switch (status) {
+
+            case NO_REPEAT:
+                break;
+            case REPEAT_PLAYLIST:
+                break;
+            case REPEAT_ONE:
+                break;
+
+
+        }
+
+    }
 
     public NowPlayingButtonBar(@NonNull Context context) {
         super(context);
@@ -53,7 +200,21 @@ public class NowPlayingButtonBar extends RelativeLayout {
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.view_button_bar, this, false);
         addView(view);
+        populateViews(view);
 
+    }
+
+    private void populateViews(View parent) {
+        this.playButton = parent.findViewById(R.id.bar_button_play);
+        this.repeatButton = parent.findViewById(R.id.bar_button_repeat);
+        this.randomButton = parent.findViewById(R.id.bar_button_random);
+        this.nextButton = parent.findViewById(R.id.bar_button_next);
+        this.previousButton = parent.findViewById(R.id.bar_button_previous);
+        this.playButton.setOnClickListener(this);
+        this.repeatButton.setOnClickListener(this);
+        this.randomButton.setOnClickListener(this);
+        this.nextButton.setOnClickListener(this);
+        this.previousButton.setOnClickListener(this);
     }
 
     @Override
@@ -62,4 +223,13 @@ public class NowPlayingButtonBar extends RelativeLayout {
         float height = getMeasuredWidth() / 8.5f;
         getLayoutParams().height = Math.round(height);
     }
+
+    public interface OnClickListener {
+        void playPressed(PlayStatus status);
+        void randomPressed(RandomStatus status);
+        void repeatPressed(RepeatStatus status);
+        void nextPressed();
+        void previousPressed();
+    }
+
 }
